da09429b9345190860ffbbb34ec9eb3df1bc38f9
Baqyt Abdesh <130103041@stu.sdu.edu.kz>
2019-07-28 20:42:21 +0600

    Add flask - version1.2

--- pre.py
+++ pre.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import numpy as np
+from flask import Flask, render_template, request
 
 file = 'krisha.csv'
 
 df = pd.read_csv(file)
 
-# print(df.head())
-
-
-
 df = df.iloc[:, :4]
-# print(df.head())
 
 x = []
 y = []
@@ -23,18 +19,8 @@ for i in range(len(df)):
 x_num = np.array(x)
 y_num = np.array(y)
 
-
-'''
-x - xmin
-xmax - xmin
-'''
-
-
-
 max_rooms = np.max(x_num[:, 0])
 min_rooms = np.min(x_num[:, 0])
-
-
 
 # print(x_num.shape[1])
 x_max_rooms = np.max(x_num[:, 1])
@@ -50,36 +36,39 @@ y_num[:] = (y_num[:] - np.min(y_num[:])) / (np.max(y_num[:]) - np.min(y_num[:]))
 for i in range(1, x_num.shape[1]):
     x_num[:, i] = (x_num[:, i] - np.min(x_num[:, i])) / (np.max(x_num[:, i]) - np.min(x_num[:, i]))
 
-
-alpha = 0.1
+alpha = 0.01
 m = len(x_num)
-
 
 thetas = np.random.rand(1, 4)
 y_num.shape = (y_num.shape[0], 1)
 thetas = thetas.T
-
-
-
-
 
 for i in range(5000):
     h = np.dot(x_num, thetas)
     j = 1 / m * np.dot(x_num.T, (h - y_num))
     thetas = thetas - alpha * j
 
+app = Flask(__name__)
 
+@app.route('/')
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        rooms = float(request.form['rooms'])
+        area = float(request.form['area'])
+        year = float(request.form['year'])
+        rooms_norm = (rooms - x_min_rooms) / (x_max_rooms - x_min_rooms)
+        area_norm = (area - x_min_area) / (x_max_area - x_min_area)
+        year_norm = (year - x_min_year) / (x_max_year - x_min_year)
+        inputs = np.array([1.0, rooms_norm, area_norm, year_norm])
+        inputs.shape = (4, 1)
+        mul = np.dot(thetas.T, inputs)
+        out = mul * y_max - mul * y_min + y_min
+        out = int(out[0])
+        mini = int(out * 0.9)
+        maxi = int(out * 1.1)
+        return render_template('result.html', mini=mini, maxi=maxi)
+    return render_template('main_page.html')
 
-
-
-my_rooms = float(input('rooms: '))
-my_area = float(input('area: '))
-my_year = float(input('year: '))
-my_inp = np.array([1, (my_rooms - x_min_rooms) / (x_max_rooms - x_min_rooms), (my_area - x_min_area) / (x_max_area - x_min_area), (my_year - x_min_year) / (x_max_year - x_min_year)])
-
-
-y_norm = (np.dot(thetas.T, my_inp))
-
-out = y_norm * y_max - y_norm * y_min + y_min
-print(out)
-
+if __name__ == '__main__':
+    app.run(debug=True, port=7070)
